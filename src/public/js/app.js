b7c590a7b8223aeb97a74d772de8122a213c3395
DooPal2000 <kkyub1313@gmail.com>
2024-01-13 16:59:22 +0900

    소켓 통신 구현, (app.js 불러오기 실패)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,14 @@
-alert("hi")
+alert('hi!')
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open",() => {
+    console.log("Connected to Server OOOOO");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Just Got this: ", message, "from the Server");
+});
+
+socket.addEventListener("close",() => {
+    console.log("Connect from Server XXXXX");
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, {WebSocketServer} from "ws";
 import express from "express";
 import path from "path";
 
@@ -5,7 +7,7 @@ const app = express();
 const __dirname = path.resolve();
 
 app.set('view engine', "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 
 app.get("/",(req,res) => {
@@ -17,5 +19,21 @@ app.get("/*", (req,res) => {
 });
 
 
-const handleListen = () => console.log('Listening on port 3000')
-app.listen(3000, handleListen);
+const handleListen = () => console.log('Listening on port 3000');
+
+//http 서버와 webSocket 서버를 둘 다 만든 경우임
+const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.send("hello!")
+    console.log(socket);
+});
+
+server.listen(3000,handleListen);
+
+
+
+// app.listen(3000, handleListen);
