be7dec52c917d32015135e60a4866e1d3b985314
Kyoung Yeob <kkyub1313@gmail.com>
2024-01-18 17:54:33 +0900

    Socket io 챕터 마무리
    Bonus + Socket.io admin page 학습 +

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
 import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 // import WebSocket, {WebSocketServer} from "ws";
 
@@ -33,17 +34,71 @@ app.get("/*", (req,res) => {
 const handleListen = () => console.log('Listening on port 3000');
 
 const httpServer = http.createServer(app);
-const wsServer =  new Server(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    }
+});
+
+instrument(wsServer, {
+    auth: false,
+    mode: "development",
+});
+  
+  
+
+function publicRooms(){
+    const {sockets : { adapter : {sids, rooms}}} = wsServer;
+    // const sids = wsServer.sockets.adapter.sids;
+    // const rooms = ws.socket.adapter.rooms;
+
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 
 wsServer.on("connection", socket => {
-    // ⚠️ done() 함수는 백엔드에서 실행 x 프론트에서 o 실행 되는 것이다 (헷갈림 주의)
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
+        console.log(`Socket Event : ${event}`);
+    });
+    // ⚠️ done() 함수는 백엔드에서 실행 x 프론트에서 실행 되는 것이다 (헷갈림 주의)
     // 결과적으로, "backend done" 문구는 브라우저 콘솔에 출력된다.
     socket.on("enter_room", (roomName, done) => {
-        console.log(roomName);
-        setTimeout(() => {
-            done(`hello ${roomName}'s user, It's me. (from the backend)`); 
-        }, 5000);
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        console.log(socket.rooms);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
+
+    socket.on("new_message", (msg,room,done)=>{
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+        done();
+    });
+
+    socket.on("nickname", nickname=> {
+        socket["nickname"] = nickname;
+    });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room)-1));
+    });    
+
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
 });
 
 
