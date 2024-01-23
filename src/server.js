7b51c233f4fbb247b29b0cb0096dbc9873c12c24
Kyoung Yeob <kkyub1313@gmail.com>
2024-01-23 18:06:29 +0900

    IceCandidate #3.7 중간까지 완료
    ICE 절차에서는 여러 경로를 탐색하고, 그 중에서 최적의 경로를 선택합니다. 이 때 탐색하는 경로가 ICECANDIDATE 입니다.

--- src/server.js
+++ src/server.js
@@ -27,16 +27,24 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
   
 
-const handleListen = () => console.log('Listening on port 3000');
 
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
-wsServer.on("connection", socket => {
-    socket.on("join_room", (roomName,done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome");
-    })
-})
-
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+      socket.join(roomName);
+      socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+      socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+      socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice",ice);
+    });
+  });
+  
+const handleListen = () => console.log('Listening on port 3000');
 httpServer.listen(3000, handleListen);
